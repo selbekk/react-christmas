763fab78fe3dfde073a435a7accbb215a2d3ebf0
Kristofer Selbekk <selbeezy@gmail.com>
2017-11-27 22:24:40 +0100

    Add a hover effect to cards also

--- src/Card.js
+++ src/Card.js
@@ -7,9 +7,17 @@ const OuterContainer = styled.li`
   background: linear-gradient(to right, #f00, #d00);
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
   border-radius: 4px;
-  flex: 0 0 calc(50% - 6px);
-  margin: 0 0 12px 0;
+  flex: 0 0 calc(50% - 12px);
+  margin: 0 0 24px 0;
   padding: 12px;
+  transition: all .1s ease-out;
+  will-change: box-shadow transform;
+
+  &:focus, &:hover {
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.25);
+      outline: none;
+      transform: translate(-1px, -1px);
+  }
 
   ${breakpoints.mediumUp} {
     flex-basis: 23%;
