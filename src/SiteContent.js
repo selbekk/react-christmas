31a37d230fd5f50e65bca1c2b3ed3fc85616dd73
Kristofer Selbekk <selbeezy@gmail.com>
2017-11-27 18:13:50 +0100

    Add sticky footer

---
+++ src/SiteContent.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const SiteContent = styled.main`
+    flex-grow: 1;
+`;
+
+export default SiteContent;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import { injectGlobal } from "styled-components";
+import styled, { injectGlobal } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import SiteHeader from "./SiteHeader";
+import SiteContent from "./SiteContent";
 import SiteFooter from "./SiteFooter";
 
+/* eslint-disable no-unused-expressions */
 injectGlobal`
   @import url('https://fonts.googleapis.com/css?family=Covered+By+Your+Grace');
   body {
@@ -21,14 +23,23 @@ injectGlobal`
     box-sizing: inherit;
   }
 `;
+/* eslint-enable no-unused-expressions */
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
 
 const Root = () => (
   <BrowserRouter>
-    <div>
+    <AppContainer>
       <SiteHeader />
-      <App />
+      <SiteContent>
+        <App />
+      </SiteContent>
       <SiteFooter />
-    </div>
+    </AppContainer>
   </BrowserRouter>
 );
 
