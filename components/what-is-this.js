627e20005cad669efd550857e572047f7a6d34b5
Stian Fredrikstad <stian.fredrikstad@bekk.no>
2018-10-24 18:46:56 +0200

    What is this rewrite

--- components/what-is-this.js
+++ components/what-is-this.js
@@ -5,18 +5,16 @@ const WhatIsThis = () => (
     <SectionTitle>What is React.Christmas?</SectionTitle>
     <Paragraph>
       This site aims to{' '}
-      <strong>educate, entertain and get people interested in React</strong>.
+      <strong>educate, entertain and get people interested in Security</strong>.
       It's for Christians, Muslims, Jews, Mormons and even the odd atheist!
     </Paragraph>
     <Paragraph>
-      This page and its content is created by me - Kris Selbekk, a simple
-      Norwegian that enjoys writing about React, JavaScript and a slew of other
-      topics.
+      This page and its content is created by the Security Group at Bekk Consulting.
     </Paragraph>
     <Paragraph>
-      You can find me on Twitter if you want to say hello, and you can find the
+      You can find the
       source code of this project on{' '}
-      <LinkText href="https://github.com/selbekk/react-christmas">
+      <LinkText href="https://github.com/stian-fredrikstad/security-christmas">
         GitHub
       </LinkText>, if you want to fix a typo or improve the article for other
       readers.
