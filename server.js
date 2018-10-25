a49ec701a14ecacad48731d8d50d08e35438f5f5
Stian Fredrikstad <stian.fredrikstad@bekk.no>
2018-10-25 23:18:31 +0200

    Add helmet to set default security headers

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const next = require('next');
 const compression = require('compression');
+const helmet = require('helmet')
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -11,6 +12,9 @@ const runTheTrap = async () => {
     await app.prepare();
     const server = express();
 
+    //Enable helmet to set security headers
+    server.use(helmet())
+    
     // gzip it!
     server.use(compression());
 
