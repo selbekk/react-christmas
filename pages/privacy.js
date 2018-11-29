9fad531b7592d79508a9d324bf76e78980d2da1a
Stian Fredrikstad <stian.fredrikstad@bekk.no>
2018-11-29 16:37:27 +0100

    Add privacy statement

--- pages/privacy.js
+++ pages/privacy.js
@@ -2,23 +2,35 @@ import Page from '../components/page';
 import ContentContainer from '../components/content-container';
 import Center from '../components/center';
 import {
-  PageTitle,
-  LeadParagraph,
-  Paragraph,
-  LinkText,
+    PageTitle,
+    LeadParagraph,
+    Paragraph,
+    LinkText,
 } from '../components/typography';
+import DocumentWithStyledComponents from './_document';
 
 const PrivacyPage = () => (
-  <Page title="Privacy policy">
-    <ContentContainer>
-      <Center>
-        <PageTitle>Privacy policy</PageTitle>
-      </Center>
-      <Paragraph>
-        Privacy is important to us, and we do not want to track any information about you.
-      </Paragraph>
-    </ContentContainer>
-  </Page>
+    <Page title="Privacy policy">
+        <ContentContainer>
+            <Center>
+                <PageTitle>Privacy policy</PageTitle>
+                <LeadParagraph>TL;DR: We use Google Analytics.</LeadParagraph>
+            </Center>
+            <Paragraph>
+                Privacy is important to me, but I don't really need to track a ton of
+                information about you. I use Google Analytics to track information about
+                where you're from and what kind of device you're using etc - but{' '}
+                <strong>that's it</strong>.
+            </Paragraph>
+            <Paragraph>
+                If you need to get in touch with us, please do so on by sending an{' '}
+                <LinkText href="mailto:sikkerhet-2018@bekk.no">email</LinkText>.
+            </Paragraph>
+            <Paragraph>
+                Not sure what else to write here. Hope you enjoy your day! 👋
+            </Paragraph>
+        </ContentContainer>
+    </Page>
 );
 
 export default PrivacyPage;
