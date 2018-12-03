6421d77a5027065f379cb34f2ec3c8ea471a9c07
Ingar Almklov <ingara@gmail.com>
2018-12-03 09:27:26 +0100

    Add highlight.js

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,13 @@ class DocumentWithStyledComponents extends Document {
         <Head>
           {this.props.styleTags}
           <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
+          <link
+            rel="stylesheet"
+            href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/styles/default.min.css"
+          />
+          <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/highlight.min.js" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/languages/elm.min.js" />
+          <script>hljs.initHighlightingOnLoad();</script>
         </Head>
         <body>
           <Main />
